79fa938a00a87808f45fb2dbec68e3d962a5840f
Duque <iker_duque@euneiz.com>
2025-04-03 20:07:10 +0200

    Brillo+Volumen

---
+++ Assets/Script/Menu/LogicaBrillo.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicaBrillo : MonoBehaviour
+{
+
+
+    public Slider slider;
+    public float sliderValue;
+    public Image panelBrillo;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+
+        panelBrillo.color = new Color( panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b , sliderValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSlider(float valor)
+    {
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        panelBrillo.color = new Color (panelBrillo.color.r , panelBrillo.color.g , panelBrillo.color.b , sliderValue );
+
+    }
+}
--- Assets/Script/Menu/OptionsMenu.cs
+++ Assets/Script/Menu/OptionsMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
@@ -6,12 +7,17 @@ public class OptionsMenu : MonoBehaviour
     public Scrollbar volumeScrollbar;
     public AudioSource audioSource;
 
+    public Scrollbar BrightnessScrollbar;
+    public Light mainLight;
 
     private void Start()
     {
         //cargar el volumen guardado 
         volumeScrollbar.value = PlayerPrefs.GetFloat("GameVolume", 1f);
         audioSource.volume = volumeScrollbar.value;
+
+        BrightnessScrollbar.value = PlayerPrefs.GetFloat("GameBrightness", 1f);
+        mainLight.intensity = BrightnessScrollbar.value;
     }
 
     public void ChangeVolume()
@@ -21,4 +27,15 @@ public class OptionsMenu : MonoBehaviour
         PlayerPrefs.Save();// guarda el valor guardado por el jugador
     }
     
+    public void ChangeBrightness()
+    {
+        mainLight.intensity = BrightnessScrollbar.value * 2;
+        PlayerPrefs.SetFloat ("GameBrightness",  BrightnessScrollbar.value);
+        PlayerPrefs.Save();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("menu");
+    }
 }
